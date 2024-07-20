de0bb7ee500579c79ae4a1c563ef71018bc0a4d5
yesimdemir <yesimdmr000@gmail.com>
2024-07-20 16:54:57 +0300

    Stock-Traking-with-Microsoft-Azure
    A C# .NET and MVC project where a grocery store can manage its customers, products, and categories of products and keep its data in a cloud service (Microsoft Azure).

---
+++ StokMVC/StokMVC/Controllers/MusteriController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokMVC.Models.Entity;
+
+namespace StokMVC.Controllers
+{
+    public class MusteriController : Controller
+    {
+		// GET: Musteri
+		MvcDbStok db = new MvcDbStok();
+		public ActionResult Index(string p)
+        {
+			var degerler = from d in db.TBLMUSTERILER select d;
+			if (!string.IsNullOrEmpty(p))
+			{
+				degerler=degerler.Where(m=>m.MUSTERIAD.Contains(p));
+			}
+			return View(degerler.ToList());
+			//var degerler = db.TBLMUSTERILER.ToList();
+			//return View(degerler);
+		}
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+		public ActionResult YeniMusteri(TBLMUSTERILER p1)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View("YeniMusteri");
+			}
+			db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult SIL(int id)
+		{
+			var musteri = db.TBLMUSTERILER.Find(id);
+			db.TBLMUSTERILER.Remove(musteri);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult MusteriGetir(int id)
+		{
+			var mus = db.TBLMUSTERILER.Find(id);
+			return View("MusteriGetir", mus);
+		}
+
+		public ActionResult Guncelle(TBLMUSTERILER p1)
+		{
+			var mstr = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+			mstr.MUSTERIAD = p1.MUSTERIAD;
+			mstr.MUSTERISOYAD = p1.MUSTERISOYAD;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ StokMVC/StokMVC/Controllers/ÜrünController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokMVC.Models.Entity;
+
+namespace StokMVC.Controllers
+{
+    public class ÜrünController : Controller
+    {
+		// GET: Ürün
+		MvcDbStok db = new MvcDbStok();
+		public ActionResult Index()
+        {
+			var degerler = db.TBLURUNLER.ToList();
+			return View(degerler);
+        }
+
+		[HttpGet]
+		public ActionResult YeniUrun()
+		{
+			List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+											 select new SelectListItem
+											 {
+												 Text = i.KATEGORIAD,
+												 Value = i.KATEGORIID.ToString(),
+											 }).ToList();
+			ViewBag.dgr=degerler;
+			return View();
+		}
+
+		[HttpPost]
+
+		public ActionResult YeniUrun(TBLURUNLER p1)
+		{
+			var ktg=db.TBLKATEGORILER.Where(m=>m.KATEGORIID==p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+			p1.TBLKATEGORILER = ktg;
+			db.TBLURUNLER.Add(p1);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult SIL(int id)
+		{
+			var urun = db.TBLURUNLER.Find(id);
+			db.TBLURUNLER.Remove(urun);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult UrunGetir(int id)
+		{
+			var urun = db.TBLURUNLER.Find(id);
+
+			List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+											 select new SelectListItem
+											 {
+												 Text = i.KATEGORIAD,
+												 Value = i.KATEGORIID.ToString(),
+											 }).ToList();
+			ViewBag.dgr = degerler;
+
+
+			return View("UrunGetir",urun);
+		}
+
+		public ActionResult Guncelle(TBLURUNLER p1)
+		{
+			var urn = db.TBLURUNLER.Find(p1.URUNID);
+			urn.URUNAD= p1.URUNAD;
+			urn.MARKA= p1.MARKA;
+			urn.FIYAT= p1.FIYAT;
+			urn.STOK= p1.STOK;
+			//urn.URUNKATEGORI= p1.URUNKATEGORI;
+
+			var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+			urn.URUNKATEGORI = ktg.KATEGORIID;
+
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
